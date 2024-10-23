8eeae89a6471356433fe4d19ed5edfff8ddc9569
k3 <kkhaingkyaw307@gmail.com>
2024-10-23 10:49:49 +0630

    ServiceDiscovery

--- src/main/java/com/jdc/kkk/ServiceDiscoveryApplication.java
+++ src/main/java/com/jdc/kkk/ServiceDiscoveryApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//ServiceDiscovery(Eureka)
+
 @EnableEurekaServer
 @SpringBootApplication
 public class ServiceDiscoveryApplication {
